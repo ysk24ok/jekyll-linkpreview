01df0a907044967b72604450267d71113a576f84
Yusuke NISHIOKA <yusuke.nishioka@retrieva.jp>
2019-03-27 00:54:26 +0900

    First commit

---
+++ lib/jekyll/linkpreview.rb
@@ -0,0 +1,8 @@
+require "jekyll/linkpreview/version"
+
+module Jekyll
+  module Linkpreview
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/linkpreview/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Linkpreview
+    VERSION = "0.1.0"
+  end
+end
